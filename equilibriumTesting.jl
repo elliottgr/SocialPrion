e416c387af587e7539f8993720929a8c978fcbb1
elliott <elliottgreene1@gmail.com>
2023-05-01 12:42:02 -0400

    Further work on the equilibrium file. The default paramters show that there are biologically feasible parameter values that will ensure prion persistence in a glucose limited model, but not when glucose is non-limiting

--- equilibriumTesting.jl
+++ equilibriumTesting.jl
@@ -17,12 +17,13 @@ begin
 	r = 1.0
 	G = 0.5
 	g = 0.5
+	K_B = 1.0
 	K_y = 1.0
 	K_G = 1.0
-	H = 0.0
+	H = 0.1
 	B = 1.0
-	p = 0.1
-	E = 0.0
+	p = 0.2
+	E = 0.1
 	
 	## Plotting parameters
 
@@ -32,25 +33,53 @@ begin
 
 
 	## Base model
-	function dGdt(r, B, G, g, K_y, K_G, H, p, E)
-		return r*G*(1-((G+g)/K_y)) + (H * g) - (B*p*G) 
-	end
-
-	function dBdt(r, B, G, g, K_y, K_G, H, p, E)
-		return r*B*(1-(B/K_G)) - (B*G*E)
+	
+	function dBdt(r, B, G, g, K_B, K_y, K_G, H, p, E)
+		return r*B*(1-(B/K_B)) - (B*G*E)
 	end
 	
-	function dgdt(r, B, G, g, K_y, K_G, H, p, E)
+	function dGdt(r, B, G, g, K_B, K_y, K_G, H, p, E)
+		return r*G*(1-((G+g)/K_y)) + (H * g) - (B*p*G) 
+	end
+	
+	function dgdt(r, B, G, g, K_B, K_y, K_G, H, p, E)
 		return r*g*(1-((G+g)/K_y)) + (B*p*G) - (H*g)
 	end
 
-	## Plotting
-	plot()
-	plot!(N_min:N_step:N_max, [dBdt(r, N, G, g, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "B")
-	
-	plot!(N_min:N_step:N_max, [dGdt(r, B, N, g, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "G")
+	## Glucose limited model
 
-	plot!(N_min:N_step:N_max, [dgdt(r, B, G, N, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "g")
+	function dBdt_G(r, B, G, g, K_B, K_y, K_G, H, p, E)
+		return r*B*(1-(B/K_B)) - (B*G*E)
+	end
+	
+	function dGdt_G(r, B, G, g, K_B, K_y, K_G, H, p, E)
+		return r*G*(1 - ((G+B)/K_G) - ((g+G)/K_y)) + (H * g) - (B*p*G) 
+	end
+
+	function dgdt_G(r, B, G, g, K_B, K_y, K_G, H, p, E)
+		return r*g*(1 - ((g+G)/K_y)) + (B*p*G) - (H*g)
+	end
+	
+	## Plotting
+	p1 = plot(title = "Base Model", ylim = (-0.5, 0.5), legend = :none)
+	p2 = plot(title = "Glucose Limited Model", ylim = (-0.5, 0.5), yformatter = :none)
+	
+	plot!(p1, N_min:N_step:N_max, [dBdt(r, N, G, g, K_B, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "B")
+	
+	plot!(p1, N_min:N_step:N_max, [dGdt(r, B, N, g, K_B, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "G")
+
+	plot!(p1, N_min:N_step:N_max, [dgdt(r, B, G, N, K_B, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "g")
+
+	plot!(p2, N_min:N_step:N_max, [dBdt_G(r, N, G, g, K_B, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "B")
+
+	plot!(p2, N_min:N_step:N_max, [dGdt_G(r, B, N, g, K_B, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "G")
+
+	plot!(p2, N_min:N_step:N_max, [dgdt_G(r, B, G, N, K_B, K_y, K_G, H, p, E) for N in N_min:N_step:N_max], label = "g")
+	
+	plot(p1, p2, layout = (1,2))
+
+
+	# plot([base_B, base_G, base_g, glucose_B, glucose_G, glucose_G], layout = (2,1))
 end
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -1041,6 +1070,6 @@ version = "1.4.1+0"
 
 # ╔═╡ Cell order:
 # ╠═ccac70b4-e82f-11ed-1fd5-0f61af1d45c8
-# ╠═a128efad-199c-4ad4-bda3-c7ce80a9a780
+# ╟─a128efad-199c-4ad4-bda3-c7ce80a9a780
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
